bd9f8e6cadf00afeb9796b59eaad8faec3a16731
nic001122 <123776405+nic001122@users.noreply.github.com>
2023-01-27 23:11:17 +0100

    added movement

---
+++ Assets/Behaviour.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Behaviour : MonoBehaviour
+{
+    public float Speed = 23;
+    public Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float HorizontalAxis = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(HorizontalAxis * Speed * Time.deltaTime, 0);
+        transform.position = transform.position + movement;
+    }
+}
