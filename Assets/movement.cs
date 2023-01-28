306633de5f20eb00c9d8fe91ea55f9bf873ea7fd
nic001122 <123776405+nic001122@users.noreply.github.com>
2023-01-28 14:51:41 +0100

    added level

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Behaviour : MonoBehaviour
+public class movement : MonoBehaviour
 {
-    public float Speed = 23;
-    
+    public float Speed;
+  
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,10 +16,10 @@ public class Behaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         float VerticalAxis = Input.GetAxis("Vertical");
-        Vector3 movement2 = new Vector3(0,VerticalAxis * Speed * Time.deltaTime,0);
+        Vector3 movement2 = new Vector3(0, VerticalAxis * Speed * Time.deltaTime, 0);
         transform.position = transform.position + movement2;
         print(VerticalAxis);
+
     }
 }
